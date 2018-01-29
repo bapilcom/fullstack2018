ab6c5b96dfad61d6261cc01aed9d56e91adef0bc
Teo Gebhard <teo.gebhard@sujuwa.fi>
2018-01-29 22:46:03 +0200

    refaktorointi komponenteiksi

--- reactin-alkeet/src/index.js
+++ reactin-alkeet/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Kurssi = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+};
+
+const Sisalto = (props) => {
+    return (
+        <div>
+            <p>{props.osa1} {props.tehtavia1}</p>
+            <p>{props.osa2} {props.tehtavia2}</p>
+            <p>{props.osa3} {props.tehtavia3}</p>
+        </div>
+    )
+};
+
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.lkm} tehtävää</p>
+    )
+};
+
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys';
     const osa1 = 'Reactin perusteet';
@@ -12,11 +34,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>{kurssi}</h1>
-            <p>{osa1} {tehtavia1}</p>
-            <p>{osa2} {tehtavia2}</p>
-            <p>{osa3} {tehtavia3}</p>
-            <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>
+            <Kurssi kurssi={kurssi}/>
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+            <Yhteensa lkm={tehtavia1 + tehtavia2 + tehtavia3}/>
         </div>
     )
 };
