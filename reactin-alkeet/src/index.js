ec05567de0a6a7d7b55de4b21fd925e2b660c445
Teo Gebhard <teo.gebhard@sujuwa.fi>
2018-02-05 22:21:09 +0200

    2.5 erillinen moduuli

--- reactin-alkeet/src/index.js
+++ reactin-alkeet/src/index.js
@@ -1,38 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Kurssi = (props) => {
-    return (
-        <div>
-            <h1>{props.kurssi.nimi}</h1>
-            <Sisalto osat={props.kurssi.osat} />
-            <Yhteensa osat={props.kurssi.osat} />
-        </div>
-    )
-};
-
-const Osa = (props) => {
-    return (
-        <p>{props.nimi} {props.tehtavamaara}</p>
-    )
-};
-
-const Sisalto = (props) => {
-    return (
-        <div>
-            {props.osat.map(osa =>
-                <Osa key={osa.id} nimi={osa.nimi} tehtavamaara={osa.tehtavia}/>
-            )}
-        </div>
-    )
-};
-
-const Yhteensa = (props) => {
-    const tehtavaSum = props.osat.reduce((accumulator, osa) => accumulator + osa.tehtavia, 0);
-    return (
-        <p>yhteensä {tehtavaSum} tehtävää</p>
-    )
-};
+import Kurssi from './components/Kurssi'
 
 const App = () => {
     const kurssit = [
